3294df82025540f30949f1299d730c6348c05b26
Leon Davis <leon.davis@lvl-up.uk>
2018-06-29 16:42:07 +0100

    Move Commandline::Return class in to separate file and write test for it.

--- bin/support/ruby/lib/utils/commandline.rb
+++ bin/support/ruby/lib/utils/commandline.rb
@@ -1,39 +1,7 @@
 require 'colorize'
 require 'open3'
-
+require_relative 'commandline/return'
 module Commandline
-  class Return
-    attr_reader :stdout, :stderr, :exit_code
-
-    def initialize(stdout:, stderr:, exit_code:)
-      @stdout = normalise(stdout)
-      @stderr = normalise(stderr)
-      @exit_code = exit_code
-    end
-
-    def error?
-      exit_code != 0
-    end
-
-    def to_s
-      <<~OUTPUT
-        EXIT CODE: #{exit_code}
-
-        STDOUT:
-        #{stdout}
-
-        STDERR:
-        #{stderr}
-      OUTPUT
-    end
-
-    private
-
-    def normalise(string)
-      string.chomp.strip
-    end
-  end
-
   def run(command)
     stdout, stderr, status = Open3.capture3("bash -c '#{command}'")
     Return.new(stdout: stdout, stderr: stderr, exit_code: status.exitstatus)
---
+++ spec/bin/support/ruby/lib/utils/commandline/return_spec.rb
@@ -0,0 +1,45 @@
+module Commandline
+  describe Return do
+    subject { described_class.new(stdout: "stdout\n", stderr: "stderr\n", exit_code: 0) }
+    describe '#initialize' do
+      it 'takes newline characters off of stdout and stderr' do
+        expect(subject.stderr).to eq(:stderr.to_s)
+        expect(subject.stdout).to eq(:stdout.to_s)
+      end
+    end
+
+    describe '#to_s' do
+      it 'gives a formatted representation of commandline output' do
+        # rubocop:disable Layout/TrailingWhitespace
+        expected_output = <<~OUTPUT
+          EXIT CODE: #{subject.exit_code}
+          
+          STDOUT:
+          #{subject.stdout}
+          
+          STDERR:
+          #{subject.stderr}
+        OUTPUT
+        # rubocop:enable Layout/TrailingWhitespace
+
+        expect(subject.to_s).to eq(expected_output)
+      end
+
+      describe '#error?' do
+        context 'exit_code is 0' do
+          subject { described_class.new(stdout: '', stderr: '', exit_code: 0) }
+          it 'returns false' do
+            expect(subject.error?).to eq(false)
+          end
+        end
+
+        context 'exit_code is not 0' do
+          subject { described_class.new(stdout: '', stderr: '', exit_code: 1) }
+          it 'returns false' do
+            expect(subject.error?).to eq(true)
+          end
+        end
+      end
+    end
+  end
+end
