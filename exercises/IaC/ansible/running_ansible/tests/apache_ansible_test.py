61c70ca294232441d15437aef3314dc659f572d4
Leon Davis <leon.davis@lvl-up.uk>
2018-07-05 17:04:27 +0100

    Updates suggested by @heathtechnical

--- exercises/IaC/ansible/running_ansible/tests/apache_ansible_test.py
+++ exercises/IaC/ansible/running_ansible/tests/apache_ansible_test.py
@@ -1,8 +1,12 @@
-import pytest, testinfra, os, paramiko
+import pytest
 
-def test_apache_installed_enabled_running(cmdopt):
-    host=testinfra.get_host("paramiko://root@" + cmdopt, ssh_config="/root/.ssh/config")
+def test_apache_installed(host):
     assert host.package("apache2").is_installed
+
+def test_apache_is_enabled_as_service(host):
     assert host.service("apache2").is_enabled
+
+def test_apache_installed_is_running(host):
     assert host.service("apache2").is_running
     assert host.socket("tcp://0.0.0.0:80").is_listening
+
--- exercises/IaC/ansible/running_ansible/tests/conftest.py
+++ exercises/IaC/ansible/running_ansible/tests/conftest.py
@@ -1,9 +1,10 @@
-import pytest
+import pytest, testinfra, paramiko
 def pytest_addoption(parser):
     parser.addoption(
         "--ansible-host", action="store"
     )
 
 @pytest.fixture
-def cmdopt(request):
-    return request.config.getoption("--ansible-host")
+def host(request):
+    hostname=request.config.getoption("--ansible-host")
+    return testinfra.get_host("paramiko://root@" + hostname, ssh_config="/root/.ssh/config")
