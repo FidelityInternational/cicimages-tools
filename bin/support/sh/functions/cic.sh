b2db7e884b7d3144dae944b932b08be224b89a2d
Leon Davis <leon.davis@lvl-up.uk>
2018-08-09 00:06:36 +0100

    dry up cic bin script

--- bin/support/sh/functions/cic.sh
+++ bin/support/sh/functions/cic.sh
@@ -23,15 +23,19 @@ function cic_bin(){
 }
 
 function cic_exports() {
-    local cic_exports="TRACKS_PATH=$(tracks_path) EXERCISES_PATH=$(exercises_path)"
-    cic_exports="SCAFFOLD_STRUCTURE=$(cic_config_dir)/exercise_scaffold.yml"
+    local cic_exports
+    cic_exports="TRACKS_PATH=$(tracks_path) EXERCISES_PATH=$(exercises_path)"
+    cic_exports="${cic_exports} SCAFFOLD_STRUCTURE=$(cic_config_dir)/exercise_scaffold.yml"
+    cic_exports="${cic_exports} CIC_COURSEWARE_VERSION=$(cic_image_version)"
+    cic_exports="${cic_exports} CIC_COURSEWARE_IMAGE=$(cic_image_repository)"
     echo "${cic_exports} SCAFFOLD_PATH=$(cic_config_dir)/exercise_scaffold"
 }
 
 function bootstrap_cic_environment(){
     # shellcheck disable=SC2016
     local rbenv_init='eval "$(rbenv init -)"'
-    local source_bin_scripts=". $(cic_bin)/.env"
+    local source_bin_scripts
+    source_bin_scripts=". $(cic_bin)/.env"
     echo "${rbenv_init} && ${source_bin_scripts} && $(cic_exports)"
 }
 
