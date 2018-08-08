88ad49b7fa000b4cb6a4d2562aa45b87f29a5846
Leon Davis <leon.davis@lvl-up.uk>
2018-08-08 23:52:01 +0100

    extract helper methods and dry up exercise bin script

--- bin/support/sh/functions/cic.sh
+++ bin/support/sh/functions/cic.sh
@@ -10,6 +10,34 @@ function exercises_path(){
     echo "${EXERCISES_PATH}"
 }
 
-function cic_exports() {
-    echo "TRACKS_PATH=$(tracks_path) EXERCISES_PATH=$(exercises_path)"
+function cic_dir(){
+    echo "/cic"
 }
+
+function cic_config_dir(){
+  echo "$(cic_dir)/.cic"
+}
+
+function cic_bin(){
+    echo "$(cic_dir)/bin"
+}
+
+function cic_exports() {
+    local cic_exports="TRACKS_PATH=$(tracks_path) EXERCISES_PATH=$(exercises_path)"
+    cic_exports="SCAFFOLD_STRUCTURE=$(cic_config_dir)/exercise_scaffold.yml"
+    echo "${cic_exports} SCAFFOLD_PATH=$(cic_config_dir)/exercise_scaffold"
+}
+
+function bootstrap_cic_environment(){
+    # shellcheck disable=SC2016
+    local rbenv_init='eval "$(rbenv init -)"'
+    local source_bin_scripts=". $(cic_bin)/.env"
+    echo "${rbenv_init} && ${source_bin_scripts} && $(cic_exports)"
+}
+
+
+CIC_CURRENT_PATH="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+# shellcheck source=bin/support/sh/functions/cic_docker.sh
+source "${CIC_CURRENT_PATH}/cic_docker.sh"
+
+
