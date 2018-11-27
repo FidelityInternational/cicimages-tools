152d052431d56c52841fa93fe1abf228a4e24314
Leon Davis <leon.davis@lvl-up.uk>
2018-11-27 16:57:15 +0000

    init must be procedural script as it is used as an entrypoint to the container

--- spec/bin/init_spec.rb
+++ spec/bin/init_spec.rb
@@ -2,7 +2,6 @@
 describe 'init' do
 
   include_context :shell_spec, script_root: 'support/bin/'
-  let(:stubbed_env) { create_stubbed_env }
 
   describe 'init' do
     it 'copies bin scripts the given location' do
@@ -14,7 +13,7 @@ describe 'init' do
       target_dir = "#{Dir.pwd}/target_bin"
       FileUtils.mkdir_p target_dir
 
-      result = execute_function("init #{target_dir}", 'BIN_PATH' => source_dir)
+      result = execute_script([target_dir], 'BIN_PATH' => source_dir)
 
       expect(result).to_not have_error
       expect(File).to exist("#{target_dir}/#{expected_bin_script}")
--- spec/support/shared_contexts/shell_spec_context.rb
+++ spec/support/shared_contexts/shell_spec_context.rb
@@ -3,13 +3,24 @@ require 'rspec/bash'
 shared_context :shell_spec do |script_root:|
   include Rspec::Bash
 
-  let(:script_path){"#{__dir__}/../../../#{script_root}/#{self.class.top_level_description}"}
+  let(:script_path) {"#{__dir__}/../../../#{script_root}/#{self.class.top_level_description}"}
+  let(:stubbed_env) {create_stubbed_env}
 
   attr_reader :last_result
+
   def execute_function(function, env_vars = {})
     stdout, stderr, status = stubbed_env.execute_function(
         script_path,
         function,
+        env_vars
+    )
+
+    @last_result = Commandline::Return.new(stdout: stdout, stderr: stderr, exit_code: status.exitstatus)
+  end
+
+  def execute_script(args = [], env_vars = {})
+    stdout, stderr, status = stubbed_env.execute(
+        "#{script_path} #{args.join}",
         env_vars
     )
 
